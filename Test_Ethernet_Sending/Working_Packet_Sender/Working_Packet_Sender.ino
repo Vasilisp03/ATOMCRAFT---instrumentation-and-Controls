19261fd92ce66be28267487a6e9df607934d3773
Owen Pang <z5462213@ad.unsw.edu.au>
2024-11-10 00:07:45 +1100

    Updated comments

--- Test_Ethernet_Sending/Working_Packet_Sender/Working_Packet_Sender.ino
+++ Test_Ethernet_Sending/Working_Packet_Sender/Working_Packet_Sender.ino
@@ -3,7 +3,10 @@
 const char* ssid     = "Optus_540FC8";
 const char* password = "lased29799mq";
 
-const char* serverIP = "192.168.0.28"; // Replace X with your Mac's IP address
+const char* serverIP = "192.168.0.28"; // Replace the last 2 digits with the IP address of your computer
+// The above was accomplished via terminal when connected to the correct wifi network
+// Input the command: ifconfig | grep "inet "
+// The IP address should come up as: 192.168.0.XX, where XX is unique to your computer
 const int serverPort = 8080;          // Port to send data to
 
 void setup() {
